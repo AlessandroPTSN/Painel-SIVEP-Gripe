0d1bc63657858cb644f2f22635a1b87d11b2cc26
Alessandro Pereira Torres De Sá Neto <alessandroptsn@yahoo.com.br>
2022-06-15 02:56:12 -0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -20,10 +20,10 @@ mapdata <- get_data_from_map(download_map_data("countries/br/br-all"))
 
 
 
-map <- read_delim("C:/Users/Neo/Desktop/map.csv", 
+map <- read_delim("/app/map.csv", 
                   delim = ";", escape_double = FALSE, trim_ws = TRUE) %>% as.data.frame()
 
-map_doses<- read_delim("C:/Users/Neo/Desktop/codigos SQL ITPS/DADOS/map_doses.csv", 
+map_doses<- read_delim("/app/map_doses.csv", 
                     delim = ";", escape_double = FALSE, trim_ws = TRUE) %>% as.data.frame()
 
 map_doses = merge(map, map_doses, by.x = "SIGLA", by.y = "NOMES") %>% select(SIGLA,UF, POPULACAO, `DOSE 1`, `DOSE 2`, `DOSE R`)
@@ -41,13 +41,13 @@ map$`%DOSE R` = map_doses$`%DOSE R`
 
 
 
-top_3_1 <- read_delim("C:/Users/Neo/Desktop/top_3_1.csv", 
+top_3_1 <- read_delim("/app/top_3_1.csv", 
                      delim = ";", escape_double = FALSE, trim_ws = TRUE) %>% as.data.frame()
 
-top_3_2 <- read_delim("C:/Users/Neo/Desktop/top_3_2.csv", 
+top_3_2 <- read_delim("/app/top_3_2.csv", 
                       delim = ";", escape_double = FALSE, trim_ws = TRUE) %>% as.data.frame()
 
-top_3_R <- read_delim("C:/Users/Neo/Desktop/top_3_R.csv", 
+top_3_R <- read_delim("/app/top_3_R.csv", 
                       delim = ";", escape_double = FALSE, trim_ws = TRUE) %>% as.data.frame()
 
 
@@ -55,10 +55,10 @@ top_3_R <- read_delim("C:/Users/Neo/Desktop/top_3_R.csv",
 
 
 
-totais <- read_delim("C:/Users/Neo/Desktop/totais.csv", 
+totais <- read_delim("/app/totais.csv", 
                      delim = ";", escape_double = FALSE, trim_ws = TRUE) %>% as.data.frame()
 
-TS_BR_UFS <- read_delim("C:/Users/Neo/Desktop/codigos SQL ITPS/DADOS/TS_BR_UFS.csv", 
+TS_BR_UFS <- read_delim("/app/DADOS/TS_BR_UFS.csv", 
                     delim = ";", escape_double = FALSE, trim_ws = TRUE) %>% as.data.frame()
 TS_BR_UFS$DATA = as.Date(TS_BR_UFS$DATA, format = "%d/%m/%Y")
 #TS_BR_UFS = TS_BR_UFS %>% arrange(TS_BR_UFS$DATA)
@@ -69,7 +69,7 @@ TS_BR_UFS = merge(TS_BR_UFS,totais, by.x = "NOME", by.y = "UF") %>% select(NOME 
 
 
 
-cruza_all <- read_delim("C:/Users/Neo/Desktop/codigos SQL ITPS/DADOS/cruza_all.csv", 
+cruza_all <- read_delim("/app/cruza_all.csv", 
                         delim = ";", escape_double = FALSE, trim_ws = TRUE) %>% as.data.frame()
 cruza_all$DATA = as.Date(cruza_all$DATA, format = "%d/%m/%Y")
 
@@ -77,7 +77,7 @@ cruza=cruza_all %>% select(DATA,UF,OBT_ZR, OBT_UMA, OBT_DAS, OBT_TRS)
 
 
 
-estados = read_delim("C:/Users/Neo/Desktop/codigos SQL ITPS/DADOS/estados.csv", 
+estados = read_delim("/app/estados.csv", 
            delim = ",", escape_double = FALSE, trim_ws = TRUE) %>% as.data.frame()
 
 
@@ -86,9 +86,9 @@ INT_OBT_ZR ,INT_OBT_UMA, INT_OBT_DAS, INT_OBT_TRS ,INT_ZR, INT_UMA, INT_DAS ,INT
 
 cor_doses;map
 
-M=read.csv("C:/Users/Neo/Desktop/codigos SQL ITPS/DADOS/M.csv",sep = ";", header = T, row.names = 1)%>%as.matrix()
+M=read.csv("/app/M.csv",sep = ";", header = T, row.names = 1)%>%as.matrix()
 
-cor_doses = read_delim("C:/Users/Neo/Desktop/codigos SQL ITPS/DADOS/corr_h_o.csv", 
+cor_doses = read_delim("/app/corr_h_o.csv", 
                        delim = ";", escape_double = FALSE, trim_ws = TRUE) %>% as.data.frame()
 
 cor_doses = cor_doses %>% arrange((UF))
